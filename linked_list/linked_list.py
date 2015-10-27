f91d2f8913ed0414877cc4464014eccb69168774
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-10-27 14:28:53 -0400

    Add print routines

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,5 +1,4 @@
 class Node(object):
-
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
@@ -13,9 +12,10 @@ class Node(object):
     def set_next(self, new_next):
         self.next_node = new_next
 
+    def __str__(self):
+        return str(self.data)
 
 class LinkedList(object):
-
     def __init__(self, head=None):
         self.head = head
 
@@ -60,3 +60,26 @@ class LinkedList(object):
             self.head = current.get_next()
         else:
             previous.set_next(current.get_next())
+
+    def has_cycle(self):
+        tortoise = None
+        hare = None
+
+        # Advance the list 
+        while tortoise and hare and hare.get_next():
+            hare = tortoise.get_next().get_next()
+            tortoise = tortoise.get_next()
+
+            if tortoise == hare:
+                return True
+
+        return False
+    
+    def __str__(self):
+        current = self.head
+        string = ''
+        while current:
+            string += str(current) + '-->'
+            current = current.get_next()
+
+        return string
