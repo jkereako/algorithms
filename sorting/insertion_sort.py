dbc2da3ce1e011a7f49e5b4ee542e069dad4a9bb
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-10-28 17:09:42 -0400

    Add more comments

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -3,7 +3,8 @@ def sort(L):
     Insertion sort is a O(n^2) algorithm in the worst case which is when the
     array to be sorted is in reverse order. However, the space complexity of
     this algorithm is O(1) because the only auxiliary space required to perform
-    an insertion sort is the temporary variable `val`.
+    an insertion sort is the temporary variable `val`. In other words, insertion
+    sort is done "in place".
     """
     # Start from the second element so we can compare the current element with
     # the previous element.
