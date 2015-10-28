aec82a64005c2f9b4c55b485d3ee3441540f812f
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-10-28 11:24:59 -0400

    Format whitespace

--- linked_list/tests.py
+++ linked_list/tests.py
@@ -10,37 +10,43 @@ class TestLinkedList(unittest.TestCase):
 
     def test_insert(self):
         self.list.insert("David")
+
         self.assertTrue(self.list.head.get_data() == "David")
         self.assertTrue(self.list.head.get_next() is None)
-
 
     def test_insert_two(self):
         self.list.insert("David")
         self.list.insert("Thomas")
+
         self.assertTrue(self.list.head.get_data() == "Thomas")
+
         head_next = self.list.head.get_next()
         self.assertTrue(head_next.get_data() == "David")
-
 
     def test_nextNode(self):
         self.list.insert("Jacob")
         self.list.insert("Pallymay")
         self.list.insert("Rasmus")
+
         self.assertTrue(self.list.head.get_data() == "Rasmus")
+
         head_next = self.list.head.get_next()
         self.assertTrue(head_next.get_data() == "Pallymay")
+
         last = head_next.get_next()
         self.assertTrue(last.get_data() == "Jacob")
-
 
     def test_positive_search(self):
         self.list.insert("Jacob")
         self.list.insert("Pallymay")
         self.list.insert("Rasmus")
+
         found = self.list.search("Jacob")
         self.assertTrue(found.get_data() == "Jacob")
+
         found = self.list.search("Pallymay")
         self.assertTrue(found.get_data() == "Pallymay")
+
         found = self.list.search("Jacob")
         self.assertTrue(found.get_data() == "Jacob")
 
@@ -48,9 +54,12 @@ class TestLinkedList(unittest.TestCase):
     def test_searchNone(self):
         self.list.insert("Jacob")
         self.list.insert("Pallymay")
+
         # make sure reg search works
         found = self.list.search("Jacob")
+
         self.assertTrue(found.get_data() == "Jacob")
+
         with self.assertRaises(ValueError):
             self.list.search("Vincent")
 
@@ -78,7 +87,6 @@ class TestLinkedList(unittest.TestCase):
         self.list.delete("Jacob")
         self.assertTrue(self.list.head.get_next() is None)
 
-
     def test_delete_value_not_in_list(self):
         self.list.insert("Jacob")
         self.list.insert("Pallymay")
@@ -86,7 +94,6 @@ class TestLinkedList(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             self.list.delete("Sunny")
-
 
     def test_delete_empty_list(self):
         with self.assertRaises(ValueError):
@@ -97,8 +104,10 @@ class TestLinkedList(unittest.TestCase):
         self.list.insert("Cid")
         self.list.insert("Pallymay")
         self.list.insert("Rasmus")
+
         self.list.delete("Pallymay")
         self.list.delete("Cid")
+
         self.assertTrue(self.list.head.next_node.get_data() == "Jacob")
 
 if __name__ == '__main__':
