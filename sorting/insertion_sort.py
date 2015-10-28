d65e831fe5d5175c372efbf98a9567eddf95db3e
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-10-28 14:29:52 -0400

    Add useful comments to insertion sort.

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,12 +1,17 @@
-def sort(list):
-    for i in range(1, len(list)):
-        tmp = list[i]
+def sort(L):
+    # Start from the second element so we can compare the current element with
+    # the previous element.
+    for i in range(1, len(L)):
+        val = L[i]
         k = i
 
-        while k > 0 and tmp < list[k - 1]:
-            list[k] = list[k - 1]
+        # Start from the current position of the array and iterate backwards
+        # toward the beginning of the array comparing adjascent values as you
+        # go.
+        while k > 0 and val < L[k - 1]:
+            L[k] = L[k - 1]
             k -= 1
 
-        list[k] = tmp
+        L[k] = val
 
-    return list
+    return L
--- sorting/insertion_sort_tests.py
+++ sorting/insertion_sort_tests.py
@@ -1,7 +1,7 @@
 from insertion_sort import sort
 import unittest
 
-class TestMergeSort(unittest.TestCase):
+class TestInsertionSort(unittest.TestCase):
     def setUp(self):
         self.list = [9, 21, 3, 19, 290, 39, 343, 1 , 4, 6, 64, 3, 2, 13]
         self.expected = [1, 2, 3, 3, 4, 6, 9, 13, 19, 21, 39, 64, 290, 343]
