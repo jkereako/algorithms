7246b5b2e87d356a8de1d0e5ec52cd5ad62122fd
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-11-04 10:32:22 -0500

    Make binary search more Pythonic
    I removed the indexes for "high" and "low" since we can dynamically
    resize the array.

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,23 +1,27 @@
-def binary_search(list, element, low, high):
+def binary_search(L, key):
     """
-    Chop a list in half, inspect it.
+    Chop a L in half, inspect it.
     """
 
-    # Account for cases in which the list is 0 or 1 element
-    if high - low < 2:
-        return list[low] == element or list[high] == element
+    # The list is empty, hence, the key does not exist.
+    if len(L) == 0:
+        return False
 
-    # Find the middle element
-    mid = low + (high - low) // 2
+    # The list only contains 1 element, test if it is the key
+    if len(L) == 1:
+        return L[0] == key
 
-    # We found the element
-    if list[mid] == element:
+    # Find the middle key
+    mid = len(L) // 2
+
+    # We found the key
+    if L[mid] == key:
         return True
 
-    # If the middle element is greater than the subject, then search the lower
-    # half of the list.
-    if list[mid] > element:
-        return binary_search(list, element, low, mid - 1)
+    # If the middle element is greater than the key, then search the lower
+    # half of L.
+    if L[mid] > key:
+        return binary_search(L[:mid], key)
 
     # Otherwise, search the upper half
-    return binary_search(list, element, mid + 1, high)
+    return binary_search(L[mid:], key)
--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -9,14 +9,24 @@ class TestBinarySearch(unittest.TestCase):
         self.low = 0
 
     def test_for_element_in_list(self):
-        found = binary_search.binary_search(self.list, 69, self.low, self.high)
+        found = binary_search.binary_search(self.list, 69)
+
         self.assertTrue(found)
 
     def test_for_element_not_in_list(self):
-        found = binary_search.binary_search(self.list, 100001, self.low, self.high)
+        found = binary_search.binary_search(self.list, 100001)
+
         self.assertFalse(found)
 
     def test_list_of_one_element(self):
-        found = binary_search.binary_search(self.list, 100001, 0, 1)
+        self.list = [1]
+        found = binary_search.binary_search(self.list, 1)
+
+        self.assertTrue(found)
+
+    def test_empty_list(self):
+        self.list = []
+        found = binary_search.binary_search(self.list, 1)
+
         self.assertFalse(found)
 
