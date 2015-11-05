7101868c666299bd03e127ddb5a88c37206ad641
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-11-05 17:10:24 -0500

    Add clarifying comments to selection sort.

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,20 +1,31 @@
-def sort(list):
-    """Assumes that L is a list of elements that can be compared
-       using >.  Sorts L in ascending order"""
-    for i in range(len(list) - 1):
-        #Invariant: list[:i] is sorted
+def sort(L):
+    """
+    Selection sort is an in-place comparison sort. The algorithm divides the
+    input list into 2 parts: the sublist of items already sorted, which is built
+    from the front of the list toward the tail, and the sublist of items yet to
+    be sorted. See the example below:
+
+    [64, 25, 12, 22, 11]    # Initial state
+    [11, 25, 12, 22, 64]    # sorted sublist = [11]
+    [11, 12, 25, 22, 64]    # sorted sublist = [11, 12]
+    [11, 12, 22, 25, 64]    # sorted sublist = [11, 12, 22]
+    [11, 12, 22, 25, 64]    # sorted sublist = [11, 12, 22, 25]
+    [11, 12, 22, 25, 64]    # sorted sublist = [11, 12, 22, 25, 64]
+    """
+
+    for i in range(len(L) - 1):
+        #Invariant: L[:i] is sorted
         min_idx = i
-        min_val= list[i]
+        min_val= L[i]
         j = i + 1
 
-        while j < len(list):
-            if min_val > list[j]:
+        while j < len(L):
+            if min_val > L[j]:
                 min_idx = j
-                min_val= list[j]
+                min_val= L[j]
             j += 1
 
-        temp = list[i]
-        list[i] = list[min_idx]
-        list[min_idx] = temp
+        temp = L[i]
+        L[i] = list[min_idx]
+        L[min_idx] = temp
 
-    return list
