c4721b8cbd676a1246a9d86418b6aa09bca15ecf
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-11-05 20:45:15 -0500

    Add comments to quicksort

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,4 +1,20 @@
 def sort(L):
+    """
+    Developed in 1959 by Tony Hoare, quicksort is still a commonly used
+    algorithm.
+
+    Performance
+    ===========
+    Worst:      O(n^2)
+    Average:    O(n log n)
+    Best:       O(n log n)
+
+    Space
+    =====
+    Worst:      O(n)
+    Average:    O(n log n)
+    """
+
     if len(L) <= 1:
         return L
 
