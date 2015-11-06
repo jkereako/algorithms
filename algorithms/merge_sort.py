6b5b10a18465eb9de4329fa8f8b7d2bf957eca42
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-11-05 20:23:11 -0500

    Add comments to merge sort

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -1,34 +1,46 @@
-def sort(list, lt = lambda x, y: x < y):
-    """Returns a new sorted list containing the same elements as L"""
-    if len(list) < 2:
-        return list[:]
+def sort(L, compare = lambda x, y: x < y):
+    """
+    Mergesort is a famous example of a divide and conquer algorithm invented in
+    1945. It divides a list into 2 parts, sorts each part individually and then
+    merges the 2 lists.
+
+    Performance
+    ===========
+    Worst:      O(n log n)
+    Avereage:   O(n log n)
+    Best:       O(n log n)
+    """
+
+    # If L is fewer than 2 elements, then return a copy of the list
+    if len(L) < 2:
+        return list(L)
 
     # Divide and conquer!
-    middle = int(len(list)/2)
-    left = sort(list[:middle], lt)
-    right = sort(list[middle:], lt)
+    mid = len(L) // 2
+    left = sort(L[:mid], compare)
+    right = sort(L[mid:], compare)
 
-    return merge(left, right, lt)
+    return merge(left, right, compare)
 
-def merge(left, right, lt):
-    """Assumes left and right are sorted lists.
-     lt defines an ordering on the elements of the lists.
-     Returns a new sorted(by lt) list containing the same elements
-     as (left + right) would contain."""
+def merge(left, right, compare):
     result = []
     i,j = 0, 0
+
     while i < len(left) and j < len(right):
-        if lt(left[i], right[j]):
+        if compare(left[i], right[j]):
             result.append(left[i])
             i += 1
         else:
             result.append(right[j])
             j += 1
+
     while (i < len(left)):
         result.append(left[i])
         i += 1
+
     while (j < len(right)):
         result.append(right[j])
         j += 1
+
     return result
 
